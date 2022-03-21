24c105bb9aacf214fea1049eb2e9f90948225c11
GeniusXiao213 <100718931+GeniusXiao213@users.noreply.github.com>
2022-03-21 14:33:38 +0100

    Update list.h

--- LIST/list.h
+++ LIST/list.h
@@ -17,14 +17,14 @@ class sequence
     ~sequence();
     sequence(const sequence<key,Info>);
     Info getElement(Const key&);
-    bool insertAfter(const key& k,const info& i,const key& where,int accurance=1);
-    bool insertBefore(const Key& k,const info& i,const key& where,int accurance=1);
+    bool insertAfter(const key& k,const info& i,const key& where,int occurance=1);
+    bool insertBefore(const Key& k,const info& i,const key& where,int occurance=1);
 //insert elements in the middle of list
     void insertFront(const key&,const info&);
     void insertBack(const key&,const info&);
 //insert elements in the front/back of the linked list
-    bool delete(const key& which,int accurance=1);
-    bool search(const key& what, int accurance);
+    bool delete(const key& which,int occurance=1);
+    bool search(const key& what, int occurance=1);
     
     sequence<key,info>& operator=(const sequence<key, info>&);
     sequence<key,info>& operator+(const sequence<key, info>&) const;
@@ -36,6 +36,22 @@ class sequence
 
 template<typename key,typename info>
 bool sequence<key,info>::insertAfter(const key& k,const info& i,const key& where,
-int accurance=1){
-    
+int occurance=1){
+        node *current;
+        node *newNode;
+        newNode= new node;
+        newNode->info=i;
+        newNode->key=k;
+        current=head;
+        int occ=0;
+        while(current!=nullptr)
+        {
+            if(current->key=where)
+            {
+                occ++;
+            }
+
+        }
+        return false;
 }
+
